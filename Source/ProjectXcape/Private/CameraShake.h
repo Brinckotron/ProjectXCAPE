efadcfbf0bfc9b74d7ef072865ec395a4c5a075c
François Brinck-Boucher <146009172+Brinckotron@users.noreply.github.com>
2025-08-21 16:49:24 -0400

    Port to UE 5.6
    updated UCameraShake to new 5.6 system

--- Source/ProjectXcape/Private/CameraShake.h
+++ Source/ProjectXcape/Private/CameraShake.h
@@ -1,21 +1,18 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Shakes/DefaultCameraShakeBase.h"
+#include "Camera/CameraShakeBase.h" 
 #include "CameraShake.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class UCameraShake : public UDefaultCameraShakeBase
+class UCameraShake : public UCameraShakeBase
 {
 	GENERATED_BODY()
 
 public:
 	/*UFUNCTION(BlueprintImplementableEvent)
 	void ChangeDuration();*/
-	
 };
